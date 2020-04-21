884a01c1b5f642b508c50bf486843886fcbc1ae7
mehrdad-dev <mehrdad.dev@yahoo.com>
2020-04-21 18:48:35 +0430

    add SetMainMenu, and SetFullScreen

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"piano/pkg/screens"
 
 	"fyne.io/fyne"
@@ -12,10 +13,22 @@ import (
 const preferenceCurrentTab = "currentTab"
 
 func main() {
-	a := app.NewWithID("com.giano.demo")
+	a := app.NewWithID("com.jami.demo")
 	a.SetIcon(theme.FyneLogo())
 
-	w := a.NewWindow("Giano")
+	w := a.NewWindow("Jami")
+	w.SetMainMenu(fyne.NewMainMenu(
+		fyne.NewMenu("Contorol",
+			fyne.NewMenuItem("Paste", func() { w.Clipboard() }),
+			fyne.NewMenuItem("Force Quit", func() { os.Exit(1) }),
+		),
+		fyne.NewMenu(
+			"View",
+			fyne.NewMenuItem("Small Screen", func() { w.SetFullScreen(false) }),
+			fyne.NewMenuItem("Full Screen", func() { w.SetFullScreen(true) }),
+		),
+	),
+	)
 	w.SetMaster()
 
 	tabs := widget.NewTabContainer(
@@ -27,8 +40,11 @@ func main() {
 	tabs.SetTabLocation(widget.TabLocationLeading)
 	tabs.SelectTabIndex(a.Preferences().Int(preferenceCurrentTab))
 	w.SetContent(tabs)
-	w.Resize(fyne.NewSize(700, 400))
-	//w.SetFixedSize(true)
+	w.SetFullScreen(true)
+	w.SetPadded(true)
+
+	// w.Resize(fyne.NewSize(700, 400))
+	// w.SetFixedSize(true)
 
 	w.ShowAndRun()
 	a.Preferences().SetInt(preferenceCurrentTab, tabs.CurrentTabIndex())
